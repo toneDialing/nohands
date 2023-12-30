bd3c254c495eb1a63dc4afdfd0144e10086a04ff
Matthew Rathkey <rathkeym@gmail.com>
2023-12-30 00:34:15 -0700

    Stylize individual bars in menu bar upon hover (design TBD)

--- menu_hover.js
+++ menu_hover.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.header_menu_icon').onmousemove = function() {
         document.querySelector('.header_menu_icon').style.backgroundColor = 'purple';
+        document.querySelectorAll('.bar').forEach(function(bar) {
+            bar.style.border = "1px solid #0000FF";
+        });
     };
     document.querySelector('.header_menu_icon').onmouseleave = function() {
         document.querySelector('.header_menu_icon').style.backgroundColor = 'black';
+        document.querySelectorAll('.bar').forEach(function(bar) {
+            bar.style.border = 'none';
+        });
     };
 });
