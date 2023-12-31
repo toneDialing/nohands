e41df0257a680c696d92d67a844e462f2ac3274b
Matthew Rathkey <rathkeym@gmail.com>
2023-12-30 17:32:04 -0700

    Added onclick transition to menu icon

--- menu_hover.js
+++ menu_hover.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.header_menu_icon').onmousemove = function() {
-        document.querySelector('.header_menu_icon').style.backgroundColor = 'purple';
+        /* document.querySelector('.header_menu_icon').style.backgroundColor = 'purple'; */
         document.querySelectorAll('.bar').forEach(function(bar) {
-            bar.style.border = "1px solid #0000FF";
+            bar.style.boxShadow = "0 0 0.5rem #FFF";
+            /* bar.style.border = "1px solid #0000FF"; */
         });
     };
     document.querySelector('.header_menu_icon').onmouseleave = function() {
         document.querySelector('.header_menu_icon').style.backgroundColor = 'black';
         document.querySelectorAll('.bar').forEach(function(bar) {
-            bar.style.border = 'none';
+            bar.style.boxShadow = 'none';
+            /* bar.style.border = 'none'; */
         });
+    };
+    document.querySelector('.header_menu_icon').onclick = function() {
+        /* document.querySelectorAll('.bar').forEach(function(bar) {
+            bar.style.backgroundColor = 'green';
+        }); */
+        document.querySelector('.header_menu_icon').classList.toggle('menu_clicked');
     };
 });
