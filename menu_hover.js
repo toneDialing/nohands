d3cc6461e06867d8022ba876beed7a6db70e7ec8
Matthew Rathkey <rathkeym@gmail.com>
2023-12-31 17:50:32 -0700

    Restructure code and rename classes/IDs
    Restructured code, added a test image, and changed a lot of classes to
    IDs while also renaming them for clarity. Things are still in progress
    though and I need to add JS to make the header bar scroll.

--- menu_hover.js
+++ menu_hover.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.header_menu_icon').onmousemove = function() {
-        /* document.querySelector('.header_menu_icon').style.backgroundColor = 'purple'; */
+    document.querySelector('#menu_icon').onmousemove = function() {
+        /* document.querySelector('#menu_icon').style.backgroundColor = 'purple'; */
         document.querySelectorAll('.bar').forEach(function(bar) {
             bar.style.boxShadow = "0 0 0.5rem #FFF";
             /* bar.style.border = "1px solid #0000FF"; */
         });
     };
-    document.querySelector('.header_menu_icon').onmouseleave = function() {
-        document.querySelector('.header_menu_icon').style.backgroundColor = 'black';
+    document.querySelector('#menu_icon').onmouseleave = function() {
+        document.querySelector('#menu_icon').style.backgroundColor = 'black';
         document.querySelectorAll('.bar').forEach(function(bar) {
             bar.style.boxShadow = 'none';
             /* bar.style.border = 'none'; */
         });
     };
-    document.querySelector('.header_menu_icon').onclick = function() {
+    document.querySelector('#menu_icon').onclick = function() {
         /* document.querySelectorAll('.bar').forEach(function(bar) {
             bar.style.backgroundColor = 'green';
         }); */
-        document.querySelector('.header_menu_icon').classList.toggle('menu_clicked');
-        document.querySelector('.header_nav_list').classList.toggle('menu_clicked');
+        document.querySelector('#menu_icon').classList.toggle('menu_clicked');
+        document.querySelector('#nav_container').classList.toggle('menu_clicked');
     };
 });
