1017c63b2f6a1229a3b8fca1a95e6b59cfa20bbc
Matthew Rathkey <rathkeym@gmail.com>
2024-01-02 14:31:03 -0700

    Make header sticky regardless of window size
    Fixed sticky_header.js to keep the header sticking to the top of the
    page when scrolling beneath it, regardless of the window size and thus
    the size of the image above it. This was done by recalculating the
    initial position of the header whenever the window is resized.

--- sticky_header.js
+++ sticky_header.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.getElementById('sticky_header_container');
-    const initial_position = header.offsetTop;
+    let initial_unscrolled_position = header.offsetTop;
+
+    //Recalculate initial_unscrolled_position whenever window is resized
+    window.addEventListener('resize', function() {
+        initial_unscrolled_position = header.offsetTop;
+    });
+
     window.addEventListener('scroll', function() {
-        if (this.scrollY > initial_position) {
+        if (this.scrollY > initial_unscrolled_position) {
             header.classList.add('stick_on_scroll');
         }
         else {
