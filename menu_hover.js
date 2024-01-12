cab84541b33fa675eb91910c4aee3a2d153df642
Matthew Rathkey <rathkeym@gmail.com>
2024-01-11 23:15:31 -0700

    Tidy up menu_hover.js
    Removed comments and replaced querySelector with getElementById

--- menu_hover.js
+++ menu_hover.js
@@ -1,22 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#menu_icon').onmousemove = function() {
-        /* document.querySelector('#menu_icon').style.backgroundColor = 'purple'; */
+    document.getElementById('menu_icon').onmousemove = function() {
         document.querySelectorAll('.bar').forEach(function(bar) {
             bar.style.boxShadow = "0 0 0.5rem #FFF";
-            /* bar.style.border = "1px solid #0000FF"; */
         });
     };
-    document.querySelector('#menu_icon').onmouseleave = function() {
+    document.getElementById('menu_icon').onmouseleave = function() {
         document.querySelectorAll('.bar').forEach(function(bar) {
             bar.style.boxShadow = 'none';
-            /* bar.style.border = 'none'; */
         });
     };
-    document.querySelector('#menu_icon').onclick = function() {
-        /* document.querySelectorAll('.bar').forEach(function(bar) {
-            bar.style.backgroundColor = 'green';
-        }); */
-        document.querySelector('#menu_icon').classList.toggle('menu_clicked');
-        document.querySelector('#nav_container').classList.toggle('menu_clicked');
+    document.getElementById('menu_icon').onclick = function() {
+        document.getElementById('menu_icon').classList.toggle('menu_clicked');
+        document.getElementById('nav_container').classList.toggle('menu_clicked');
     };
 });
